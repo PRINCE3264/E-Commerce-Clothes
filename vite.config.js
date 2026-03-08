cb640f3acf7ad78941d0c5ab63f8af6ebecf8969
Prince Kumar Vidyarthi <princekumarvidyarthi4@gmail.com>
2026-03-08 11:09:10 +0530

    Fix vite allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: ['e-commerce-clothes-xlzx.onrender.com']
   }
 })
